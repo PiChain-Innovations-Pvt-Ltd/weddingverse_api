2e3ad89107ffa446df38519a66d19797676767a6
Pritish Das <pritish@pichainlabs.com>
2025-05-21 14:06:12 +0530

    Updated collection name

--- app/services/mongo_service.py
+++ app/services/mongo_service.py
@@ -2,5 +2,8 @@ from pymongo import MongoClient
 from app.config import settings
 
 # Initialize MongoDB client using settings
-client = MongoClient(settings.mongo_uri)
+try:
+    client = MongoClient(settings.mongo_uri)
+except:
+    client = MongoClient("mongodb://localhost:27017")
 db = client[settings.database_name]
